75c34bf4aaa2df0cc49d14406b640af94997935c
Meltoz <meltoz@outlook.fr>
2024-06-10 20:12:42 +0200

    initial commit

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, {useRef} from 'react';
+import gsap from 'gsap';
+import {useGSAP} from '@gsap/react';
+import './Hero.css';
 
-class Hero extends React.Component{
+const Hero = () => {
+    const title = useRef()
 
-    render(){
-        return (
-            <div>test</div>
-        )
-    }
-}
+    useGSAP(() =>
+        gsap.to(title.current, {opacity: 1, duration:.8, ease: 'power1.in'}),
+    )
+
+    return (
+        <div className="h-[100vh] bg-black">
+            <div className="h-[96vh] flex flex-col gap-10 items-center justify-center bg-black">
+                <p ref={title}
+                    className="text-red-600 font-extrabold text-7xl border-[14px] border-red-600 px-10 py-4 opacity-0"
+                >COMPANY</p>
+            </div>
+            <p className="text-lg text-center scroll">[↓ Scroll to discover ↓]</p>
+        </div>
+    );
+};
 
 export default Hero;
