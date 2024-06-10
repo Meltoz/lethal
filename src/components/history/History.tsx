2d9a42932f86a10791e3d9938e315a2032bb2964
Meltoz <meltoz@outlook.fr>
2024-06-10 22:34:26 +0200

    CI/CD

--- src/components/history/History.tsx
+++ src/components/history/History.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import WantYou from '@assets/img/wantyou.webp'
 
 const History = () => {
     return (
@@ -7,7 +6,6 @@ const History = () => {
             <h2 className="text-red-600/70 text-5xl font-bold text-center">Notre Histoire</h2>
             <div className="grid grid-cols-12 grid-rows-10 mt-10">
                 <p className="text-neutral-200 col-start-2 col-span-5 row-start-2 row-end-2 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aperiam, blanditiis consequuntur culpa cupiditate dolorem eveniet fuga fugit, impedit iusto laboriosam laudantium magnam molestiae quis sapiente sed sequi veniam, veritatis!</p>
-                <img src={WantYou} alt="company want you" />
             </div>
         </div>
     );
